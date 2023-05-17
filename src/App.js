0b9bb404d3a83369efe109cbcbac203a61b1b354
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2023-05-17 22:45:51 +0700

    update navbar 2023-05-17-003

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ import FlashCard from "./components/FlashCard/FlashCard";
 import WordGuessGame from "./components/WordGuessGame/WordGuessGame";
 import Navigation from "./components/Navigation/Navigation";
 import Navbar from "./components/Navbar/Navbar";
+import NavbarTest from "./components/Navbar/NavbarTest";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavbarTest />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/test" element={<Test />} />
